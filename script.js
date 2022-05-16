1ffa70e14309122ea0fb4f7ae0aab7ef4905f917
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-16 08:38:06 +0500

    Basic understanding of DOM

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+/* 
+⭐⭐⭐-------------DOM Manipulation------------⭐⭐⭐
+
+---- Example how to Select  class and change their values.----
+--- This process is known as DOM Manupulation---
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number !!😃';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+
+*/
+
+
