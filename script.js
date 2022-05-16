1534b08583faa839b425523b896f3d8baece2735
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-16 09:40:10 +0500

    True False conditions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 ⭐⭐⭐-------------DOM Manipulation------------⭐⭐⭐
 
 ---- Example how to Select  class and change their values.----
---- This process is known as DOM Manupulation---
+----This process is known as DOM Manupulation----
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number !!😃';
@@ -16,4 +16,52 @@ document.querySelector('.guess').value = 23;
 
 */
 
+//  ⭐⭐⭐---------Starting Main Logic---------⭐⭐⭐
 
+
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretnumber;
+
+
+let score = 20;
+document.querySelector('.check').addEventListener
+('click', function() {
+
+    const guessbox = Number(document.querySelector('.guess').value);
+
+    if (!guessbox){
+        document.querySelector('.message').textContent = '⛔ No Number inserted ❗❗❗';
+    }
+
+    else if (guessbox === secretnumber){
+        document.querySelector('.message').textContent = 'Correct Number !!🎉🎉😃🎉🎉';
+    }
+
+    else if (guessbox > secretnumber){
+
+       if(score>1 ){ 
+        document.querySelector('.message').textContent = 'Too High !! ✖️✖️✖️';
+        score--;
+        document.querySelector('.score').textContent = score;
+       }
+       else {
+        document.querySelector('.message').textContent = 'Game Over!!☠️☠️☠️';
+        document.querySelector('.score').textContent = 0;
+       }     
+    }
+
+    else if (guessbox < secretnumber){
+       
+        if(score>1){
+        document.querySelector('.message').textContent = 'Too Low !! ✖️✖️✖️';
+        score--;
+        document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = 'Game Over!!☠️☠️☠️';
+            document.querySelector('.score').textContent = 0;
+           }     
+    }
+
+
+});
